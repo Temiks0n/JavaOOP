3d9dfee521a20b524872e60a84b0f01374e31bc5
artemsukhov <gow55511@yandex.ru>
2019-08-16 22:23:46 +0700

    final version

--- ArrayListHome/src/ArrayListHome.java
+++ ArrayListHome/src/ArrayListHome.java
@@ -16,11 +16,11 @@ public class ArrayListHome {
         }
 
         // 2 задание
-        ArrayList<Integer> oddIntegers = new ArrayList<>(Arrays.asList(2, 7, 8, 4, 1, 9, 23, 13, 4, 5, 15, 13, 23));
+        ArrayList<Integer> integerNumbers = new ArrayList<>(Arrays.asList(2, 7, 8, 4, 1, 9, 23, 13, 4, 5, 15, 13, 23));
 
-        for (int i = 0; i < oddIntegers.size(); i++) {
-            if (oddIntegers.get(i) % 2 == 0) {
-                oddIntegers.remove(i);
+        for (int i = 0; i < integerNumbers.size(); i++) {
+            if (integerNumbers.get(i) % 2 == 0) {
+                integerNumbers.remove(i);
                 i--;
             }
         }
@@ -28,14 +28,14 @@ public class ArrayListHome {
         // 3 задание
         ArrayList<Integer> integersWithoutRepetition = new ArrayList<>();
 
-        for (Integer e : oddIntegers) {
+        for (Integer e : integerNumbers) {
             if (!integersWithoutRepetition.contains(e)) {
                 integersWithoutRepetition.add(e);
             }
         }
 
         System.out.println("Файл содержит строки: " + strings);
-        System.out.println("Нечетные числа: " + oddIntegers);
+        System.out.println("Нечетные числа: " + integerNumbers);
         System.out.println("Числа без повторений: " + integersWithoutRepetition);
     }
 }
